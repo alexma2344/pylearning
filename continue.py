938820c07b22a594a84adeeee3e272849d068c71
Alejandro Marin <alexma2344@gmail.com>
2020-04-06 17:22:03 +0100

    continue statements

---
+++ continue.py
@@ -0,0 +1,34 @@
+#continue test
+
+userWord = input("Enter your word: ")
+userWord = userWord.lower()
+
+# for letter in userWord:
+    # if letter == "A":
+        # continue
+    # elif letter == "E":
+        # continue
+    # elif letter == "I":
+        # continue
+    # elif letter == "O":
+        # continue
+    # elif letter == "U":
+        # continue
+    # else:
+        # print(letter)
+
+
+
+for i in userWord:
+    if i == "a":
+        continue
+    elif i == "e":
+        continue
+    elif i == "i":
+        continue
+    elif i == "o":
+        continue
+    elif i == "u":
+        continue
+    else:
+        print(i)
